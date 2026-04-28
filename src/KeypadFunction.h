575c6a542f13f9cd91fbfd26b2b4ce7434ee5e1c
Edward Blaha <69668372+Edward-Blaha@users.noreply.github.com>
2026-04-28 21:50:39 +1000

    Create KeypadFunction.h

---
+++ src/KeypadFunction.h
@@ -0,0 +1,27 @@
+#ifndef KEYPADFUNCTION_H
+#define KEYPADFUNCTION_H
+
+#include <Arduino.h>
+#include <Adafruit_Keypad.h>
+
+static const int KEYPAD_ROWS = 4;
+static const int KEYPAD_COLS = 3;
+
+
+struct KeypadDef {
+  byte rowPins[KEYPAD_ROWS];
+  byte colPins[KEYPAD_COLS];
+};
+
+class KeypadFunction{
+public:
+  KeypadFunction( KeypadDef* keypadDef );
+  char getPressedKey();
+  void begin();
+  void tick();
+
+private:
+  Adafruit_Keypad mainKeypad;
+  
+};
+#endif
